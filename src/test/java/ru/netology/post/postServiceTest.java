90a84804c503e0f754765f8809a3a3dcff311193
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2024-02-10 01:30:13 +0600

    new body

--- src/test/java/ru/netology/post/postServiceTest.java
+++ src/test/java/ru/netology/post/postServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology.post;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class postServiceTest {
 
@@ -12,14 +13,14 @@ public class postServiceTest {
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("address") // отправляемые данные (заголовки и query можно выставлять аналогично)
                 // Выполняемые действия
                 .when()
                 .post("/post")
                 // Проверки
                 .then()
                 .statusCode(200)
-                //.body(/* --> ваша проверка здесь <-- */)
+                .body("data",equalTo("address"));
         ;
 
     }
